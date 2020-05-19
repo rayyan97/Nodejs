a1aec9846516f6cca1eede6cfc4236c2b28f2906
rayyan97 <ray9167086870@gmail.com>
2020-05-19 18:35:47 +0530

    fetch api url changed

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ city.textContent=''
 weatherForm.addEventListener('submit', (e) => {
  e.preventDefault()
  const location = search.value
- fetch('http://localhost:3000/weather?address='+location).then((response) => {
+ fetch('https://weather-app-v-1.herokuapp.com/weather?address='+location).then((response) => {
  response.json().then((data) => {
  if (data.message) {
  temp.textContent = data.message
